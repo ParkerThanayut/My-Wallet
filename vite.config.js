4d44effe918fb2763a91cab2d5c16632a8d44e17
Parker <114637634+ParkerThanayut@users.noreply.github.com>
2025-11-20 03:42:02 +0700

    Refine comments in vite.config.js
    Updated comments in vite.config.js for clarity.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false, // ปิด source map เพื่อแก้ปัญหา CSP eval และ Error กวนใจ
+    sourcemap: false, // ปิดอันนี้ ปัญหาจุกจิกจะหายไป!
   },
 })
