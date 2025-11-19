eff25ae4a787f717cc05fda127bc2755ee66930f
Parker <114637634+ParkerThanayut@users.noreply.github.com>
2025-11-20 03:20:18 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false, // ปิด source map เพื่อแก้ปัญหา CSP eval และ Error กวนใจ
+  },
 })
